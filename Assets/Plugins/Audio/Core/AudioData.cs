5d9d2390e9cf16a5fb198c4c7e4c3d7d34d49ba7
VictorGoldFox <doroninvitia@gmail.com>
2024-03-25 19:08:05 +0300

    edit audio data

--- Assets/Plugins/Audio/Core/AudioData.cs
+++ Assets/Plugins/Audio/Core/AudioData.cs
@@ -6,20 +6,18 @@ namespace Plugins.Audio.Core
     [Serializable]
     public class AudioData
     {
-#if !UNITY_WEBGL || UNITY_EDITOR
         public AudioClip Clip;
-#endif
 
         public string Key;
         public string FolderPath;
         public string Name;
         public bool Preload = true;
-        
+
         public string ID => _id;
 
         [SerializeField] private string _id;
-        
-        
+
+
         public AudioData()
         {
             if (string.IsNullOrEmpty(_id))
